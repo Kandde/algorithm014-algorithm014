018716e3e5436b1c4235d2224ad4723bdf0bf7dd
zhaohongxin <zhaohongxin@gmail.com>
2020-08-13 23:01:43 +0800

    move zeroes 2

--- Week_01/src/action/movezeroes/MoveZeroes.java
+++ Week_01/src/action/movezeroes/MoveZeroes.java
@@ -1,20 +1,19 @@
 package action.movezeroes;
 
 /**
+ * 1. 0812
+ * 2. 0813 分清前后指针
  * @author zhaohongxin
  */
 public class MoveZeroes {
-    public void solution(int[] nums) {
-        int nonZeroPos = 0;
+    public void moveZeroes(int[] nums) {
+        int j = 0;
         for (int i = 0; i < nums.length; i ++) {
             if (nums[i] != 0) {
-                if (nonZeroPos != i) {
-                    nums[nonZeroPos] = nums[i];
-                    nums[i] = 0;
-                }
-                nonZeroPos ++;
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = tmp;
             }
-
         }
     }
 }
